98fbe4d766b8062fedf9338f03d99986b5a1963d
chandanch <chandan.cec107@gmail.com>
2017-12-03 21:19:30 +0530

    refactor build.js

--- build/build.js
+++ build/build.js
@@ -5,17 +5,20 @@ const webpack = require("webpack")
 const config = require("./config")
 const webpack_config = require("./webpack_prod_config")
 
-// start spinner
-const spinner = ora("building for production...")
+// use the ora spinner - the terminal spinner
+// this indicates the processing with spinner and the title
+const spinner = ora("Webpack is building the app for production")
 spinner.start()
 
-// first clean the dist folder
-// then build a new version
+// remove all the assets under the dist folder first
+// generate a new bundle again
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+    // throw if any error while removing files
     if (err) throw err
     webpack(webpack_config, function (err, stats) {
         spinner.stop()
         if (err) throw err
+        //using the node process API write to the terminal
         process.stdout.write(stats.toString({
             colors: true,
             modules: false,
@@ -24,6 +27,6 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
             chunkModules: false
         }) + "\n\n")
 
-    console.log("Build complete.\n")
+        console.log("Build complete.\n")
     })
 })
