9397f1a2234e61eef2bd08b89722268aa38cd461
chandanch <chandan.cec107@gmail.com>
2017-12-03 10:16:46 +0530

    refactor base and dev config

--- build/webpack_base_config.js
+++ build/webpack_base_config.js
@@ -4,23 +4,33 @@ const config = require("./config")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
-    "cache": true,
+    // entry files
     "entry": {
         "polyfills": path.join(__dirname, "..", "src", "polyfills.ts"),
         "vendor": path.join(__dirname, "..", "src", "vendor.ts"),
         "client": path.join(__dirname, "..", "src", "app", "root.ts"),
-        "styles": path.join(__dirname, "..", "src", "styles", "style.root.scss")
     },
+    
+    "cache": true,
     "output": {
-        "filename": "[name].js",
+       // use the build asset root as path
         "path": config.build.assetsRoot,
+        // use the same name as the existing one
+        "filename": "[name].js",
+        // check the env and select path
         "publicPath": process.env.NODE_ENV === "production"
             ? config.build.assetsPublicPath
             : config.dev.assetsPublicPath
     },
+    // acceptable extensions for resolution
     "resolve": {
         "extensions": [".ts", ".js"]
     },
+    /**
+     * Loaders used: typescript loader *sass-loader: css-loader
+     * html-loader
+     * file-loader(to load fonts)
+     */
     "module": {
         "rules": [
             {
@@ -29,7 +39,7 @@ module.exports = {
                     {
                         "loader": "awesome-typescript-loader",
                         "options": {
-                            // specify your tsconfig.json file
+                            // typescript configurations for the loader
                             "configFileName": path.join(__dirname, "..", "tsconfig.json")
                         }
                     },
@@ -37,7 +47,13 @@ module.exports = {
                 ]
             },
             {
+                // html loader
+                "test": /\.html$/,
+                "loaders": ["html-loader"]
+            },
+            {
                 "test": /\.scss|\.css$/,
+                // extract the css as text
                 "loader": ExtractTextPlugin.extract({
                     "fallback": "style-loader",
                     "use": ["css-loader", 
@@ -47,21 +63,21 @@ module.exports = {
                 })
             },
             {
-                "test": /\.html$/,
-                "loaders": ["html-loader"]
-            },
-            {
+                // load fonts using the font loader
                 "test": /\.(ttf|eot|svg|woff|woff2)$/,
                 "loader": "file-loader?name=fonts/[name].[ext]"
             }
         ]
     },
 
+    /**
+     * Plugins used: extract-text-webpack-plugin, common-chunks-webpack-plugin
+     */
     "plugins": [
-        // bundle all you styling into one style.css file
+        // all styles will be bundled on to the style.css
         new ExtractTextPlugin({ "filename": "style.css", "allChunks": true }),
         
-        // create chunks for entries
+        // creating chunks for entries
         new webpack.optimize.CommonsChunkPlugin({
             "name": ["client", "vendor", "polyfills", "styles"]
         }),
