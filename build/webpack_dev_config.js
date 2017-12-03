ad9513add0af236f40af9b971bdc5eaf01df796f
chandanch <chandan.cec107@gmail.com>
2017-12-03 11:29:44 +0530

    refactor dev_config

--- build/webpack_dev_config.js
+++ build/webpack_dev_config.js
@@ -9,24 +9,33 @@ const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin")
 const WebpackBuildNotifierPlugin = require("webpack-build-notifier")
 
 
+// add entry chunks to hot-module-relod
 Object.keys(base_webpack_config.entry).forEach(function (name) {
     base_webpack_config.entry[name] = [path.join(__dirname, "dev_client")].concat(base_webpack_config.entry[name])
 })
 
 
+// extend the config from webpack_base_config
 module.exports = merge(base_webpack_config, {
     "plugins": [
+        // defines the global constants
         new webpack.DefinePlugin({
+            // set the node env as development
             "process.env": config.dev.env
         }),
         
+        //overriding the inferred information or context
         new webpack.ContextReplacementPlugin(
+            // get the correct current folder or context for angular libraries
             /angular(\\|\/)core(\\|\/)@angular/,
             path.join(__dirname, "..", "src")
         ),
 
-        new WebpackBuildNotifierPlugin({ "title": "build complete" }),
+        // notifies webpack build errors: success or error or waring
+        // uses the title option to specify the notification title
+        new WebpackBuildNotifierPlugin({ "title": "Webpack finished building" }),
         
+        // enable the HMR
         new webpack.HotModuleReplacementPlugin(),
         
         new webpack.NoEmitOnErrorsPlugin(),
